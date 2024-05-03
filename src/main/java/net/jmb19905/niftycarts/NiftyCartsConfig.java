dba0983734daca7a48c0cb83d4d45146f848913c
jmb05 <jmb05@web.de>
2024-05-03 16:40:59 +0200

    Added Rendering supplies config

--- src/main/java/net/jmb19905/niftycarts/NiftyCartsConfig.java
+++ src/main/java/net/jmb19905/niftycarts/NiftyCartsConfig.java
@@ -14,17 +14,52 @@ public final class NiftyCartsConfig {
         return Holder.COMMON_SPEC;
     }
 
+    public static Client getClient() {
+        return Holder.CLIENT;
+    }
+
+    public static ForgeConfigSpec clientSpec() {
+        return Holder.CLIENT_SPEC;
+    }
+
     private static final class Holder {
         private static final Common COMMON;
 
         private static final ForgeConfigSpec COMMON_SPEC;
 
+        private static final Client CLIENT;
+        private static final ForgeConfigSpec CLIENT_SPEC;
+
         static {
             final Pair<Common, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Common::new);
             COMMON = specPair.getLeft();
             COMMON_SPEC = specPair.getRight();
+            final Pair<Client, ForgeConfigSpec> clientSpecPair = new ForgeConfigSpec.Builder().configure(Client::new);
+            CLIENT = clientSpecPair.getLeft();
+            CLIENT_SPEC = clientSpecPair.getRight();
         }
     }
+
+
+    public static class Client {
+        public final ForgeConfigSpec.BooleanValue renderSupplies;
+        public final ForgeConfigSpec.BooleanValue renderSupplyGear;
+        public final ForgeConfigSpec.BooleanValue renderSupplyFlowers;
+        public final ForgeConfigSpec.BooleanValue renderSupplyPaintings;
+        public final ForgeConfigSpec.BooleanValue renderSupplyWheel;
+
+        Client(final ForgeConfigSpec.Builder builder) {
+            builder.comment("Configuration to disable the rendering of certain supplies in the supply cart");
+            this.renderSupplies = builder.comment("Enables/Disables the rendering of all supplies")
+                    .define("render_supplies", true);
+            this.renderSupplyGear = builder.comment("Falls back to rendering as items if false").define("render_supply_gear", true);
+            this.renderSupplyFlowers = builder.comment("Falls back to rendering as items if false").define("render_supply_flowers", true);
+            this.renderSupplyPaintings = builder.comment("Falls back to rendering as items if false").define("render_supply_paintings", true);
+            this.renderSupplyWheel = builder.comment("Falls back to rendering as items if false").define("render_supply_wheel", true);
+        }
+
+    }
+
 
     public static class Common {
         public final CartConfig supplyCart;
